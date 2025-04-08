ca6a47338deb485abb2fb82ced82dc4da3f5e7a8
Scholar Name <scholar@kodewithklossy.com>
2025-04-08 18:49:17 -0500

    Complete practice and spicy challenge

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,24 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
-    @State private var textTitle = "What is your name?"
+    @State private var favSnack = ""
+    @State private var birthday = Date()
+    @State private var presentAlert = false
+    
     var body: some View {
         VStack {
-            Text(textTitle)
+            Text("Please complete this form.")
                 .font(.title)
             TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
-            Button("Submit Name") {
-                textTitle = "Welcome, \(name)!"
-                name = ""
+            TextField("Enter your favorite snack...", text: $favSnack)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            DatePicker(selection: $birthday, displayedComponents: [.date], label: { Text("Birthday") })
+                .datePickerStyle(.graphical)
+            Button("Submit") {
+                presentAlert = true
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
         }
         .padding()
+        .alert("Thanks!", isPresented: $presentAlert, actions: {
+            Button("OK") {
+                name = ""
+                favSnack = ""
+                birthday = Date()
+            }
+        }, message: {
+            Text("Got it! \(name)'s favorite snack is \(favSnack) and their birthday is \(birthday.formatted(date: .long, time:.omitted)).")
+        })
     }
 }
 
